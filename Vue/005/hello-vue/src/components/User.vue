16f374c37b18e183e08bac5cd63f24ebd853ac15
jimmy <JimmyMall@live.com>
2021-04-30 17:54:08 +0800

    dev0.2.6-8@sustech Vue3 005/ .vue文件的使用入门

---
+++ Vue/005/hello-vue/src/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user">
+    <h1>{{ title }}, <slot></slot> </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User3',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user{color:#ddd;}
+</style>
